16a89313e1ff5dd1a78b38b1b75da7ffbbcdd660
kasfandiyarov <kasfandiyarov@gmail.com>
2020-09-13 08:29:54 +0300

    Fight 429

--- family-archive/src/main/kotlin/net/swarmshine/familty/archive/DownloadStrategy.kt
+++ family-archive/src/main/kotlin/net/swarmshine/familty/archive/DownloadStrategy.kt
@@ -1,24 +1,43 @@
 package net.swarmshine.familty.archive
 
-class DownloadStrategy (){
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient
 
-    private val previousDownloadWasSuccessful = true
+class DownloadStrategy (
+        private val httpClientFactory: ()-> CloseableHttpClient
+): AutoCloseable {
+    companion object {
+        private val ATTEMPTS_COUNT = 5
+    }
+
+    private var previousDownloadWasSuccessful = true
+    private var httpClient = httpClientFactory()
 
     fun downloadImage(){
-        when(downloadImageByUrl()){
-            is Success -> {
-
-            }
-            is TooManyRequests -> {
-                if(previousDownloadWasSuccessful){
-                    restartHttpClient()
-                } else {
-                    wait
+        for (attempt in 1..ATTEMPTS_COUNT) {
+            when (val downloadResult = downloadImageByUrl()) {
+                is Success -> {
+                    previousDownloadWasSuccessful = true
+                    return
+                }
+                is TooManyRequests -> {
+                    if (previousDownloadWasSuccessful) {
+                        restartHttpClient()
+                    } else {
+                        Thread.sleep(downloadResult.timeout)
+                        restartHttpClient()
+                    }
+                }
+                is Error -> {
+                    previousDownloadWasSuccessful = false
                 }
             }
-
         }
+        throw Exception("Failed to download image after $ATTEMPTS_COUNT attempts")
+    }
 
+    private fun restartHttpClient() {
+        httpClient.close()
+        httpClient = httpClientFactory()
     }
 
     open class DownloadResult
@@ -28,5 +47,9 @@ class DownloadStrategy (){
 
     fun downloadImageByUrl(): DownloadResult {
         TODO()
+    }
+
+    override fun close() {
+        httpClient.close()
     }
 }
